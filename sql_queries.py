a90067b776179eadba61fd3e40e53ffbe9020925
Vicente Lizana <v.lizana.e@gmail.com>
2022-07-26 19:13:05 +0000

    Correction: Table users update level on conflict.

--- sql_queries.py
+++ sql_queries.py
@@ -83,8 +83,8 @@ INSERT INTO users
     (user_id, first_name, last_name, gender, level)
 VALUES
     (%s, %s, %s, %s, %s)
-ON CONFLICT
-    DO NOTHING;
+ON CONFLICT (user_id)
+    DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""
